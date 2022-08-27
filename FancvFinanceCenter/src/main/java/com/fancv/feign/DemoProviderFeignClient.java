d3236a53957de977ea985c323c78cbceeff49cda
Andy <winglive@163.com>
2022-08-27 20:26:41 +0800

    spring cloud code base finance init

---
+++ FancvFinanceCenter/src/main/java/com/fancv/feign/DemoProviderFeignClient.java
@@ -0,0 +1,13 @@
+package com.fancv.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "operation-service")
+public interface DemoProviderFeignClient {
+
+    @GetMapping("/echo")
+    String echo(@RequestParam("name") String name);
+
+}
