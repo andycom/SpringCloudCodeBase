3a4aca96ebe82ecba0a5375d4ac7882eb82f2d6c
Andy <winglive@163.com>
2022-08-27 20:31:21 +0800

    spring cloud code base init

---
+++ FancvOperationCenter/src/main/java/com/fancv/OperationApplicationStart.java
@@ -0,0 +1,14 @@
+package com.fancv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class OperationApplicationStart {
+    public static void main(String[] args) {
+        SpringApplication.run(OperationApplicationStart.class, args);
+    }
+}
